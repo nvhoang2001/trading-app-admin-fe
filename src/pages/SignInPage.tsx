ac6bb8c06425d1a9bed1eaf82d7c1781aa6b0e0a
Ha-Sotatek <hoang.ngo@sotatek.com>
2023-07-18 21:20:34 +0700

    feat: add auth layout

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,9 @@
+function SignInPage() {
+	return (
+		<div className="w-96 h-40 bg-white rounded-lg flex justify-center items-center">
+			<h1>Sign in</h1>
+		</div>
+	);
+}
+
+export default SignInPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,21 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 
-import VoidLayout from "@/layouts/Void";
+// import VoidLayout from "@/layouts/Void";
 import DefaultLayout from "@/layouts/Default";
+import AuthLayout from "@/layouts/Auth";
 import Homepage from "@/pages/Homepage";
+import SignInPage from "@/pages/SignInPage";
 
 export const routes: RouteObject[] = [
 	{
-		path: "/dang-nhap",
-		element: <VoidLayout />,
-		children: [],
+		path: "/sign-in",
+		element: <AuthLayout />,
+		children: [
+			{
+				index: true,
+				element: <SignInPage />,
+			},
+		],
 	},
 	{
 		path: "/",
